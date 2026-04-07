d98d18699d81d7f1725dcb615a57b14fb7d65adb
david <natube553@gmail.com>
2026-04-07 22:04:55 +0200

    scaffolding

---
+++ src/test/java/it/scan/natube/NAtubeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.scan.natube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NAtubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
