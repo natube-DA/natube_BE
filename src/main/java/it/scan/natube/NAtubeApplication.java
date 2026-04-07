caae3519b294f359ae20b883b6d554808a26fbc8
david <natube553@gmail.com>
2026-04-07 22:04:55 +0200

    scaffolding

---
+++ src/main/java/it/scan/natube/NAtubeApplication.java
@@ -0,0 +1,13 @@
+package it.scan.natube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NAtubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NAtubeApplication.class, args);
+    }
+
+}
